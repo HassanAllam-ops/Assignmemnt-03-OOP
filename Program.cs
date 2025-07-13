6a86f78130705284c26e7980e65ed291bac5561d
Hassan Allam <hassan309pro@gmail.com>
2025-07-14 02:45:41 +0300

    Part 01 (Q 01) : Design and implement a Class for the employees in a company: Notes: Employee is identified by an ID, Name, security level, salary, hire date and Gender. We need to restrict the Gender field to be only M or F [Male or Female] Assign the following security privileges to the employee (guest, Developer, secretary and DBA) in a form of Enum. We want to provide the Employee Class to represent Employee data in a string Form (override ToString ()), display employee salary in a currency format. [Use String.Format() Function].

--- Program.cs
+++ Program.cs
@@ -1,10 +1,67 @@
-﻿namespace Assignmemnt_03_OOP
+﻿using System.Globalization;
+namespace Assignmemnt_03_OOP
 {
+    #region Part 01 (Q 01)
+   //ublic enum SecurityLevel
+   //
+   //   Guest,
+   //   Developer,
+   //   Secretary,
+   //   DBA
+   //
+   //
+   //ublic class Employee
+   //
+   //   public int ID { get; set; }
+   //   public string Name { get; set; }
+   //   private char gender;
+   //   public char Gender
+   //   {
+   //       get => gender;
+   //       set
+   //       {
+   //           if (value == 'M' || value == 'F')
+   //           {
+   //               gender = value;
+   //           }
+   //           else
+   //           {
+   //               throw new ArgumentException("Gender Must Be 'M' or 'F' ");
+   //           }
+   //       }
+   //   }
+   //   public SecurityLevel Security { get; set; }
+   //   public decimal Salary { get; set; }
+   //   public DateTime HireDate { get; set; }
+   //
+   //   public Employee(int id, string name, char gendr, SecurityLevel security, decimal salary, DateTime hireDate)
+   //   {
+   //       ID = id;
+   //       Name = name;
+   //       Gender = gender;
+   //       Security = security;
+   //       Salary = salary;
+   //       HireDate = hireDate;
+   //   }
+   //
+   //   public override string ToString()
+   //   {
+   //       return $"ID: {ID}\nName: {Name}\nGender: {(Gender == 'M' ? "Male" : "Female")}\n" +
+   //          $"Security Level: {Security}\nSalary: {String.Format(CultureInfo.CurrentCulture, "{0:C}", Salary)}\n" +
+   //          $"Hire Date: {HireDate.ToShortDateString()}";
+   //
+   //   }
+   //
+    #endregion
+
+    
+
     internal class Program
-    {
-        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+
+            }
         }
-    }
+    
 }
