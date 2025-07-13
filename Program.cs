5434264bcf0cd46b7effc522cbc1dc2da0c221dc
Hassan Allam <hassan309pro@gmail.com>
2025-07-14 02:57:25 +0300

    Part 01 (Q 02) : .Develop a Class to represent the Hiring Date Data: Consisting of fields to hold the day, month and Years.

--- Program.cs
+++ Program.cs
@@ -2,61 +2,97 @@
 namespace Assignmemnt_03_OOP
 {
     #region Part 01 (Q 01)
-   //ublic enum SecurityLevel
+    public enum SecurityLevel
+    {
+        Guest,
+        Developer,
+        Secretary,
+        DBA
+    }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        private char gender;
+        public char Gender
+        {
+            get => gender;
+            set
+            {
+                if (value == 'M' || value == 'F')
+                {
+                    gender = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Gender Must Be 'M' or 'F' ");
+                }
+            }
+        }
+        public SecurityLevel Security { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime HireDate { get; set; }
+
+        public Employee(int id, string name, char gendr, SecurityLevel security, decimal salary, DateTime hireDate)
+        {
+            ID = id;
+            Name = name;
+            Gender = gender;
+            Security = security;
+            Salary = salary;
+            HireDate = hireDate;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}\nName: {Name}\nGender: {(Gender == 'M' ? "Male" : "Female")}\n" +
+               $"Security Level: {Security}\nSalary: {String.Format(CultureInfo.CurrentCulture, "{0:C}", Salary)}\n" +
+               $"Hire Date: {HireDate.ToShortDateString()}";
+
+        }
+    }
+    #endregion
+
+    #region Part 02 (Q 02)
+   //ublic class HiringDate
    //
-   //   Guest,
-   //   Developer,
-   //   Secretary,
-   //   DBA
+   //   public int Day { get; set; }
+   //   public int Month { get; set; }
+   //   public int Year { get; set; }
    //
-   //
-   //ublic class Employee
-   //
-   //   public int ID { get; set; }
-   //   public string Name { get; set; }
-   //   private char gender;
-   //   public char Gender
+   //   public HiringDate(int day, int month, int year)
    //   {
-   //       get => gender;
-   //       set
-   //       {
-   //           if (value == 'M' || value == 'F')
-   //           {
-   //               gender = value;
-   //           }
-   //           else
-   //           {
-   //               throw new ArgumentException("Gender Must Be 'M' or 'F' ");
-   //           }
-   //       }
+   //       if (!IsValidDate(day, month, year))
+   //           throw new ArgumentException("invalid date provided");
+   //       Day = day;
+   //       Month = month;
+   //       Year = year;
    //   }
-   //   public SecurityLevel Security { get; set; }
-   //   public decimal Salary { get; set; }
-   //   public DateTime HireDate { get; set; }
    //
-   //   public Employee(int id, string name, char gendr, SecurityLevel security, decimal salary, DateTime hireDate)
+   //   private bool IsValidDate(int day, int month, int year)
    //   {
-   //       ID = id;
-   //       Name = name;
-   //       Gender = gender;
-   //       Security = security;
-   //       Salary = salary;
-   //       HireDate = hireDate;
+   //       try
+   //       {
+   //           DateTime temp = new DateTime(year, month, day);
+   //           return true;
+   //       }
+   //       catch
+   //       {
+   //           return false;
+   //       }
+   //
    //   }
    //
    //   public override string ToString()
    //   {
-   //       return $"ID: {ID}\nName: {Name}\nGender: {(Gender == 'M' ? "Male" : "Female")}\n" +
-   //          $"Security Level: {Security}\nSalary: {String.Format(CultureInfo.CurrentCulture, "{0:C}", Salary)}\n" +
-   //          $"Hire Date: {HireDate.ToShortDateString()}";
-   //
+   //     DateTime date = new DateTime(Year, Month, Day);
+   //       return date.ToShortDateString();
    //   }
    //
-    #endregion
+        #endregion
 
-    
-
-    internal class Program
+        internal class Program
         {
             static void Main(string[] args)
             {
